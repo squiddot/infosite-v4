ce8e8faffd374329398689746631c79b4c45776e
Taylor <taylorcgaines@gmail.com>
2022-06-03 19:21:09 -0600

    add nuxt/strapi module

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,5 +2,9 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    buildModules: ['@nuxtjs/strapi'],
+    strapi: {
+    //    options
+    }
 
 })
