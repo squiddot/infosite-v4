7cbb1f16012565d216d13067ab89ada4d52f5f0b
Taylor <taylorcgaines@gmail.com>
2022-06-03 22:14:36 -0600

    add working gsap example

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,11 +1,38 @@
 <template>
-  <div>
-    <div class="connected-status">Connected</div>
-    <NuxtWelcome />
+  <div class="container">
+    <div class="card" v-for="card in cards" :key="card.id"></div>
   </div>
+  <NuxtWelcome></NuxtWelcome>
 </template>
 
 <script>
-import { gsap } from 'gsap';
+import gsap from 'gsap'
 
+export default {
+  data() {
+    return { cards: [1, 2, 3, 4, 5, 6] }
+  },
+  mounted() {
+    gsap.from('.card', {
+      repeat: -1,
+      duration: .7,
+      stagger: .1,
+      y: -150
+    })
+  }
+}
 </script>
+
+<style>
+.container{
+  display:flex;
+  flex-wrap:wrap;
+  justify-content:space-evenly;
+  width:400px;
+  margin:20px auto}
+
+.card{
+  width:50px;
+  height:50px;
+  background:dodgerblue}
+</style>
